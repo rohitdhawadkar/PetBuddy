3268693b54c96afd75c7434b8e3d8936393b516e
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2025-03-25 23:06:10 +0530

    solved error in dashboard

--- FrontendReact/petBreact/src/components/PetDashboard/Dashboard.tsx
+++ FrontendReact/petBreact/src/components/PetDashboard/Dashboard.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { useNavigate } from "react-router-dom";
 import { useDarkMode } from "../../contexts/DarkModeContext";
 
 const Dashboard: React.FC = () => {
-  const _navigate = useNavigate();
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const [activeTab, setActiveTab] = React.useState("appointments");
 
